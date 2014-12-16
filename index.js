b2d93f37162a77d5d557955e9d1076457aeaa6b0
Fabien O'Carroll <fabien@allou.is>
2014-12-16 12:50:34 +0000

    add auth command

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ var stripetools = require('./lib/stripetools');
 
 function create(configstr) {
   var config;
-  if (configstr[0] === '@') {
+  if (!configstr) {
+    try {
+      config = {
+        apikey: fs.readFileSync(__dirname + '/apikey').toString()
+      };
+    } catch (e) {
+      console.log('No auth supplied');
+      process.exit(1);
+    }
+  } else if (configstr[0] === '@') {
     var configfile = configstr.slice(1);
     if (configfile.indexOf('/') === -1) {
       configfile = path.parse(process.cwd() + '/' + configfile);
@@ -28,6 +37,7 @@ function create(configstr) {
   var instance = Object.create(stripetools);
   instance.config = config;
   instance.stripe = stripe(config.apikey);
+  instance.apikey = config.apikey;
 
   return instance;
 }
