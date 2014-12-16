6378cbc62ecd0a7bc7996fee8e7e8121b46bf062
Fabien O'Carroll <fabien@allou.is>
2014-12-16 10:47:30 +0000

    initial bin script

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+
+var stripe = require('stripe');
+
+var stripetools = require('./lib/stripetools');
+
+function create(configstr) {
+  var config;
+  if (configstr[0] === '@') {
+    var configfile = configstr.slice(1);
+    if (configfile.indexOf('/') === -1) {
+      configfile = path.parse(__dirname + configfile);
+    }
+    config = fs.readFileSync(configfile);
+  } else {
+    try {
+      config = JSON.parse(configstr);
+    } catch (e) {
+      config = {
+        apikey: configstr
+      };
+    }
+  }
+  
+  var instance = Object.create(stripetools);
+  instance.config = config;
+  instance.stripe = stripe(config.apikey);
+
+  return instance;
+}
+
+exports.create = create;
